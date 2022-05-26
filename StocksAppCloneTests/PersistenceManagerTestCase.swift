66b9b4d5cec15b61583e3dff972c97f47d0a091b
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-05-26 19:47:56 -0400

    Created unit tests for the initial code in persistenceManager

--- StocksAppCloneTests/PersistenceManagerTestCase.swift
+++ StocksAppCloneTests/PersistenceManagerTestCase.swift
@@ -6,29 +6,43 @@
 //
 
 import XCTest
+@testable import StocksAppClone
 
 class PersistenceManagerTestCase: XCTestCase {
+    // properties
+    var persistenceManagerHandler : PersistanceManager!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        persistenceManagerHandler = PersistanceManager.shared
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_AddSymbol()
+    {
+        let symbolToAdd = "APPL"
+        persistenceManagerHandler.addToWatchList(symbolToAdd: symbolToAdd)
+        let lastSymbolInWatchList = persistenceManagerHandler.watchList[persistenceManagerHandler.watchList.count - 1]
+        XCTAssertEqual(symbolToAdd, lastSymbolInWatchList)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    
+    func test_RemoveFromWatchList()
+    {
+        let arrayToTest = ["APPL","MSFT","AMZN"]
+        persistenceManagerHandler.watchList = arrayToTest
+        persistenceManagerHandler.removeFromWatchList(symbolToRemoveString: "APPL")
+        if(persistenceManagerHandler.watchList.contains("APPL"))
+        {
+            XCTFail()
+        }
+        else
+        {
+            // here this means that it does not contain the symbol so the test should pass
         }
     }
 
