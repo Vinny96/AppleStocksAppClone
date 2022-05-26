31ec5a491513ca7dbb38f84c3cab9a1a66db4e87
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-05-26 17:46:12 -0400

    Created initial files for application

---
+++ StocksAppClone/Controllers/WatchListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StocksAppClone
+//
+//  Created by Vinojen Gengatharan on 2022-05-26.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeVC()
+    }
+
+
+    
+    
+    // Functions
+    func initializeVC()
+    {
+        view.backgroundColor = .systemBackground
+    }
+}
+
