829865ae5e9f9353f55faad5e463cd4b7f10b877
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-05-26 19:45:13 -0400

    Created initial skeleton code for the maangers and unit tested initial code for PersistanceManager

---
+++ StocksAppClone/Managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  StocksAppClone
+//
+//  Created by Vinojen Gengatharan on 2022-05-26.
+//
+
+import Foundation
---
+++ StocksAppClone/Managers/PersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceManager.swift
+//  StocksAppClone
+//
+//  Created by Vinojen Gengatharan on 2022-05-26.
+//
+
+import Foundation
