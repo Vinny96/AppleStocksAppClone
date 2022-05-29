5cb4fca674f9d9a55d0dd08b479d8716a5423c4a
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-05-28 23:20:14 -0400

    Created extensions file which will house all the extensions used in the applications

---
+++ StocksAppClone/Other/Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Extensions.swift
+//  StocksAppClone
+//
+//  Created by Vinojen Gengatharan on 2022-05-28.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Framing Extensions
+extension UIView
+{
+    var width : CGFloat {
+        return frame.width
+    }
+    
+    var height : CGFloat {
+        return frame.height
+    }
+    
+    var top : CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom : CGFloat {
+        return top + height
+    }
+    
+    var left : CGFloat {
+        return frame.origin.x
+    }
+    
+    var right : CGFloat {
+        return left + width
+    }
+    
+}
