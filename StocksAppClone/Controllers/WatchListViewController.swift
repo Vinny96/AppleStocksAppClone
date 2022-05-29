0907de7d8bf0d997317326bbd3796d328e05e523
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-05-28 23:21:14 -0400

    Created initial code that displays Stocks title and the date vertically via stack view

--- StocksAppClone/Controllers/WatchListViewController.swift
+++ StocksAppClone/Controllers/WatchListViewController.swift
@@ -11,16 +11,84 @@ class WatchListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTitleView()
         initializeVC()
+        setupSearchController()
     }
-
-
+    
+    //MARK: - UI Variables
     
     
-    // Functions
+
+    // MARK: - UI Functions
+    private func setupTitleView()
+    {
+        
+        let stockTitleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+        stockTitleLabel.textAlignment = .left
+        stockTitleLabel.text = "Stocks"
+        stockTitleLabel.font = .systemFont(ofSize: 20, weight: .heavy)
+        
+        let dateTitleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+        dateTitleLabel.textAlignment = .left
+        dateTitleLabel.text = "DATE"
+        dateTitleLabel.font = .systemFont(ofSize: 20, weight: .heavy)
+        dateTitleLabel.textColor = .systemGray
+        
+        let titleStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+        titleStackView.addArrangedSubview(stockTitleLabel)
+        titleStackView.addArrangedSubview(dateTitleLabel)
+        titleStackView.distribution = .fillEqually
+        titleStackView.alignment = .leading
+        titleStackView.axis = .vertical
+    
+        navigationItem.titleView = titleStackView
+        
+        // we need to get the DATE to display the actual date and this will be done via a DateFormatter
+    }
+    
+    
+    // MARK: - Functions
     func initializeVC()
     {
         view.backgroundColor = .systemBackground
     }
+    
+    func setupSearchController()
+    {
+        let searchResultsVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: searchResultsVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+        
+    }
+    
 }
 
+// MARK: - Extensions
+extension WatchListViewController : UISearchResultsUpdating
+{
+    func updateSearchResults(for searchController: UISearchController)
+    {
+        guard let safeQuery = searchController.searchBar.text, !safeQuery.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        guard let resultsVC = searchController.searchResultsController as? SearchResultsViewController else {
+            return
+        }
+        // now that we have the safeQuery we can call our request function from out NetworkManager and use those results to update these results
+        
+        // we want to call the api
+        
+        
+        // we want to optimize the API calls so the API is not called with every keystroke
+        
+        // we want to update the searchResultsViewController
+        print(safeQuery)
+    }
+    
+    
+    
+    
+    
+}
