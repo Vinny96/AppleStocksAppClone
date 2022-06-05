0bc8c7fdfae65dabff521b18582cf706731eb181
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-06-05 17:22:53 -0400

    Created file that will get current Date and return it in string format

---
+++ StocksAppClone/Other/DateFormatterHandler.swift
@@ -0,0 +1,16 @@
+//
+//  DateFormatter.swift
+//  StocksAppClone
+//
+//  Created by Vinojen Gengatharan on 2022-06-05.
+//
+
+import Foundation
+
+final class DateFormatterHandler
+{
+    static let shared = DateFormatterHandler()
+    private let dateFormatter = DateFormatterHandler()
+    
+    
+}
