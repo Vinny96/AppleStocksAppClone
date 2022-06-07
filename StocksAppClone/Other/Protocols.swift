1679c586b443be2790a210024437ed360c54380e
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-06-06 20:36:40 -0400

    Added a protocol that will take care of passing data back to the WatchListViewController from SearchResultViewController

--- StocksAppClone/Other/Protocols.swift
+++ StocksAppClone/Other/Protocols.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+protocol DidSelectSearchResultCell
+{
+    func searchResultSelected(model : String) // when the model is created, the model will no longer be of type string
+}
+
+protocol NetworkCallComplete
+{
+    func retrievedData(model : String) // when the model is created, the model will no longer be of type string
+}
