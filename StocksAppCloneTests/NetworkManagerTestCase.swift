0d7c58de10b4ad4b500070d8a32c7b8af1059504
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-06-06 20:38:20 -0400

    Created test case for the search functionality of the network manager

--- StocksAppCloneTests/NetworkManagerTestCase.swift
+++ StocksAppCloneTests/NetworkManagerTestCase.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import StocksAppClone
 
 class NetworkManagerTestCase: XCTestCase {
 
@@ -17,18 +18,18 @@ class NetworkManagerTestCase: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_Search()
+    {
+        let networkManager = NetworkManager.shared
+        let stockSymbol = "Apple"
+        networkManager.search(query: stockSymbol) { searchResult in
+            switch searchResult
+            {
+            case .success(let stockSearchResult):
+                XCTAssertTrue(stockSearchResult.result.count != 0)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
         }
     }
 
