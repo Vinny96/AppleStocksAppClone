1e92c86f986a998d256a4b87eb0de4a7beeeedf4
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-07-16 14:32:09 -0400

    Created code for NetworkCallQueue struct with add and remove functionalities

--- StocksAppClone/Managers/NetworkCallQueue.swift
+++ StocksAppClone/Managers/NetworkCallQueue.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct NetworkCallQueue
+{
+    // properties
+    private let runningTask : URLSessionDataTask
+    private var underlyingArray : [URLSessionDataTask] = [URLSessionDataTask]()
+    
+    // initializer
+    init(taskToRun : URLSessionDataTask)
+    {
+        runningTask = taskToRun
+    }
+    
+    // MARK: - Functions
+    
+    // this function will add to the queue
+    internal mutating func addDataTask(newDataTaskToAdd : URLSessionDataTask)
+    {
+        // we are calling remove as we always want to check if a dataTask is alredy inside before we add a new one
+        removeDataTask()
+        underlyingArray.append(newDataTaskToAdd)
+    }
+    
+    // this function will remove from the queue
+    private mutating func removeDataTask()
+    {
+        if(underlyingArray.count == 0)
+        {
+            return
+        }
+        else
+        {
+            let dataTask  = underlyingArray[0]
+            dataTask.cancel()
+            underlyingArray.removeLast()
+        }
+    }
+
+}
