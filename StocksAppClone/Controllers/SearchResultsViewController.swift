159a474b762b9ade75d22029b71b3a3c8795d685
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-07-28 20:07:41 -0400

    Wrapped functionality in timer and also enabled cells to show the stock results

--- StocksAppClone/Controllers/SearchResultsViewController.swift
+++ StocksAppClone/Controllers/SearchResultsViewController.swift
@@ -9,21 +9,90 @@ import UIKit
 
 class SearchResultsViewController: UIViewController {
 
+    // MARK: - Properties
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(SearchResultTableViewCell.self, forCellReuseIdentifier: SearchResultTableViewCell.idenfitifer)
+        table.isHidden = true
+        return table
+    }()
+    internal var stockResults = [Stock]()
+    internal var delegate : DidSelectSearchResultCell?
+    internal var networkDelegate : NetworkCallComplete?
+    
+    // MARK: - UI Functions
+    private func setTableViewFrame()
+    {
+        tableView.frame = view.bounds
+    }
+    
+    private func setupTable()
+    {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    
+    // MARK: - System Called Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupTable()
+        initializeVC()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setTableViewFrame()
     }
-    */
+    
+    
+    
+    // MARK: - Functions
+    private func initializeVC()
+    {
+        view.backgroundColor = .systemBackground
+        networkDelegate = self
+    }
 
+}
+
+extension SearchResultsViewController : UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // here we want to return the number of rows that is equal to the number of results that are our api caller returns
+        return stockResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // here we want to create a custom tableViewCell and return it
+        let stockToUse = stockResults[indexPath.row]
+        let searchResultCell = tableView.dequeueReusableCell(withIdentifier: SearchResultTableViewCell.idenfitifer, for: indexPath) as! SearchResultTableViewCell
+        
+        var content = searchResultCell.defaultContentConfiguration()
+        content.text = stockToUse.symbol
+        content.secondaryText = stockToUse.description
+        searchResultCell.contentConfiguration = content
+        
+        return searchResultCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // so here what we want to do here is construct the model based off what was selected
+        //delegate?.searchResultSelected(model: <#T##StockSearchResult#>)
+    }
+}
+
+extension SearchResultsViewController : NetworkCallComplete
+{
+    func retrievedData(model: StockSearchResult)
+    {
+        stockResults = model.result
+        tableView.isHidden = false
+        tableView.reloadData()
+    }
 }
